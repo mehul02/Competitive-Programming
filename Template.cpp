7d631858944b0ff8bd4f99730d7899431f200d5b
mehul02 <41967207+mehul02@users.noreply.github.com>
2018-09-24 19:26:08 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,57 +1,57 @@
 	///==================================================///
 	///
 	///  Ir0nic_  ///
-    ///  MEHUL BHUTALIA  ///
+	///  MEHUL BHUTALIA  ///
 	/// Indian Institute Of Information Technology and Management,Gwalior.  ///
 	///
 	///==================================================///
-    #include<bits/stdc++.h>
+	#include<bits/stdc++.h>
 
-    ///--------------------------------------------------------------------------------
-    #define 	T() 				ll t;	cin>>t;		while(t--)
+	///--------------------------------------------------------------------------------
+	#define 	T() 				ll t;	cin>>t;		while(t--)
 	#define		ll 				    long long
 	#define		vi 				    vector<ll>
-	#define     pii                 pair<ll,ll>
+	#define     	pii                 pair<ll,ll>
 	#define 	vpair 				vector< pii >
 	#define 	fast 				ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-	#define     repp(i,a,n)         for(ll i= a ; i < n ; ++i)
-	#define     rep(i,n)            for(ll i= 0 ; i < n ; ++i)
-	#define 	p_b		 			push_back
+	#define     	repp(i,a,n)         		for(ll i= a ; i < n ; ++i)
+	#define     	rep(i,n)            		for(ll i= 0 ; i < n ; ++i)
+	#define 	p_b		 		push_back
 	#define 	p_f 				push_front
 	#define 	pop_b 				pop_back()
 	#define 	pop_f 				pop_front()
 	#define 	m_p 				make_pair
 	#define  	sumv(v)  			accumulate(v.begin(),v.end(),0);
 	#define 	sortv(v) 			sort(v.begin(),v.end())
-	#define 	sort_dec(v) 		sort(v.begin(),v.end(),greater<ll>() )
-	#define 	l_b(v,l)   		    lower_bound (v.begin(), v.end(), l)
-	#define 	u_b(v,l)   		    upper_bound (v.begin(), v.end(), l)
-	#define 	pq 					priority_queue
-	#define 	ff 					first
-	#define 	ss 					second
-	#define     len                 length()
-	#define     all(v)              v.begin(),v.end()
-    #define     mset(a,b)           memset(a,b,sizeof(a));
+	#define 	sort_dec(v) 			sort(v.begin(),v.end(),greater<ll>() )
+	#define 	l_b(v,l)   		    	lower_bound (v.begin(), v.end(), l)
+	#define 	u_b(v,l)   		    	upper_bound (v.begin(), v.end(), l)
+	#define 	pq 				priority_queue
+	#define 	ff 				first
+	#define 	ss 				second
+	#define     	len                 		length()
+	#define     	all(v)              		v.begin(),v.end()
+	#define     	mset(a,b)           		memset(a,b,sizeof(a));
 
 	///==========INPUT=============///
 	#define     sc(a)           scanf("%lld",&a);
-    #define     sc2(a,b)        scanf("%lld%lld",&a,&b);
-    #define     sc3(a,b,c)      scanf("%lld%lld%lld",&a,&b,&c);
-    #define     sc4(a,b,c,d)    scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
+	#define     sc2(a,b)        scanf("%lld%lld",&a,&b);
+	#define     sc3(a,b,c)      scanf("%lld%lld%lld",&a,&b,&c);
+	#define     sc4(a,b,c,d)    scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
 
-    ///==========PRINTING=============///
-    #define     prarr(arr)      rep(i,sizeof(arr)/sizeof(ll)) cout<<arr[i]<<" "; nl
-    #define     prvec(v)        rep(i,v.size()) cout<<v[i]<<" "; nl
-    #define     pr(x, y)        cout<<x<<"\n";
-    #define     pr2(x, y)       cout<<x<<" "<<y<<"\n";
-    #define     pr3(x, y, z)    cout<<x<<" "<<y<<" "<<z<<"\n";
+	///==========PRINTING=============///
+	#define     prarr(arr)      rep(i,sizeof(arr)/sizeof(ll)) cout<<arr[i]<<" "; nl
+	#define     prvec(v)        rep(i,v.size()) cout<<v[i]<<" "; nl
+	#define     pr(x, y)        cout<<x<<"\n";
+	#define     pr2(x, y)       cout<<x<<" "<<y<<"\n";
+	#define     pr3(x, y, z)    cout<<x<<" "<<y<<" "<<z<<"\n";
 
-    ///==========CONSTANTS=============///
-    #define     max6     1000005
-    #define     max5     100004
-    #define     max4     10003
-    #define 	mod  	 1000000007
-    #define     inf      1e9
+	///==========CONSTANTS=============///
+	#define     max6     1000005
+	#define     max5     100004
+	#define     max4     10003
+	#define 	mod  	 1000000007
+	#define     inf      1e9
 
     using namespace std;
 
