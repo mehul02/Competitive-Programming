74e98ecbf95e76de705583acf3c31490ce496e17
mehul02 <41967207+mehul02@users.noreply.github.com>
2019-10-14 14:16:19 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,12 +1,15 @@
 ///==================================================///
-
+	///
 	///  Ir0nic_  ///
-    	///  MEHUL BHUTALIA  ///
+    ///  MEHUL BHUTALIA  ///
 	/// Indian Institute Of Information Technology and Management,Gwalior.  ///
-	
+	///
 ///==================================================///
 #include<bits/stdc++.h>
-#define     T()                 ll t;    cin>>t;        while(t--)
+#ifdef LOCAL_PROJECT
+#include <prettypr.hpp>
+#endif
+#define     T()                 ll tt;    cin>>tt;        while(tt--)
 #define     ll                  long long
 #define     vi                  vector<ll>
 #define     pii                 pair<ll,ll>
@@ -14,12 +17,13 @@
 #define     fast                ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
 #define     repp(i,a,n)         for(ll i= a ; i < n ; ++i)
 #define     rep(i,n)            for(ll i= 0 ; i < n ; ++i)
+#define     rrep(i,n)           for(ll i = n - 1; i >= 0; --i)
 #define     p_b                 push_back
 #define     p_f                 push_front
 #define     pop_b               pop_back()
 #define     pop_f               pop_front()
 #define     m_p                 make_pair
-#define      sumv(v)            accumulate(v.begin(),v.end(),0);
+#define     sumv(v)             accumulate(v.begin(),v.end(),0);
 #define     sortv(v)            sort(v.begin(),v.end())
 #define     sort_dec(v)         sort(v.begin(),v.end(),greater<ll>() )
 #define     l_b(v,l)            lower_bound (v.begin(), v.end(), l)
@@ -30,7 +34,7 @@
 #define     len                 length()
 #define     all(v)              v.begin(),v.end()
 #define     mset(a,b)           memset(a,b,sizeof(a));
-
+#define     endl                '\n'
 ///==========INPUT=============///
 #define     sc(a)           cin>>a;
 #define     sc2(a,b)        cin>>a>>b;
@@ -39,18 +43,14 @@
 
 ///==========PRINTING=============///
 #define     prarr(arr)      rep(i,sizeof(arr)/sizeof(ll)) cout<<arr[i]<<" "; nl
-#define     prvec(v)        rep(i,v.size()) cout<<v[i]<<" "; nl
-#define     pr(x)           cout<<x<<"\n";
-#define     pr2(x, y)       cout<<x<<" "<<y<<"\n";
-#define     pr3(x, y, z)    cout<<x<<" "<<y<<" "<<z<<"\n";
+#define     prvec(v)        rep(i,v.size()) cout<<v[i]<<" "; nls
 
 ///==========DEBUGGING=============///
-#define     debug(a)        cout<<"*"<<a<<endl;
-#define     debug2(a,b)     cout<<"$"<<a<<" "<<b<<endl;
-#define     debug3(a,b,c)   cout<<"$"<<a<<" "<<b<<" "<<c<<endl;
-#define     bug             cout<<"#"<<endl;
-#define     nl              cout<<'\n';
-#define     endl                '\n'
+#define     debug(a)            cout<<"*"<<a<<endl;
+#define     debug2(a,b)         cout<<"$"<<a<<" "<<b<<endl;
+#define     debug3(a,b,c)       cout<<"$"<<a<<" "<<b<<" "<<c<<endl;
+#define     bug                 cout<<"#"<<endl;
+#define     nl                  cout<<endl;
 
 ///==========CONSTANTS=============///
 #define     max6     1000005
@@ -59,10 +59,19 @@
 #define     mod      1000000007
 #define     inf      1e18
 ll powermod(ll _a,ll _b,ll _m)    {ll _r=1;while(_b){if(_b%2==1)_r=(_r*_a)%_m;_b/=2;_a=(_a*_a)%_m;}return _r;}
-
 using namespace std;
+template < typename T > void pr (const T& t) {
+	cout << t << "\n";
+}
+
+template < typename T, typename U, typename ... ARGS >
+void pr (const T& t, const U& u, const ARGS& ... args) {
+	cout << t << " ";
+	pr (u, args ...);
+}
+
 int main()
 {
     fast
-
-}
+	
+}	
